8f2597dee23b8ca2668de3444318a8a36c9c5b87
Fernando Barros Horta <151946784+fernandocbhorta@users.noreply.github.com>
2025-07-13 17:23:55 -0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$database = 'class869_horta';
-$host = 'localhost';
-$username = 'root';
-$password = '';
+$database = 'YOUR_DATABASE_NAME';
+$host = 'YOUR_HOST';
+$username = 'YOUR_USER';
+$password = 'YOUR_PASSWORD';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $username, $password, [
